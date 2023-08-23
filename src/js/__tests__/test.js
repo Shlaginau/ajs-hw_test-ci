5f1a260f8df3166b2d9edaa0d20615a8a8bb404f
Uliana Shlagina <shlaginau@gmail.com>
2023-08-24 00:20:58 +0700

    second commit

--- src/js/__tests__/test.js
+++ src/js/__tests__/test.js
@@ -1,12 +1,11 @@
-import getHealthStatus from '../healthStatus.js';
+import getHealthStatus from '../healthStatus';
 
 test.each([
   ['Маг', 51, 'healthy'],
   ['Маг', 50, 'wounded'],
-  ['Маг', 15, 'wounded'], 
+  ['Маг', 15, 'wounded'],
   ['Маг', 10, 'critical'],
-])
-('testing getHealthStatus function with name %s and health %i', (name, health, expected) => {
-  const result = getHealthStatus(health);
+])('testing getHealthStatus function with name %s and health %i', (name, health, expected) => {
+  const result = getHealthStatus({ name, health });
   expect(result).toBe(expected);
 });
--- src/js/healthStatus.js
+++ src/js/healthStatus.js
@@ -1,10 +1,8 @@
-
 export default function getHealthStatus(health) {
-    if (health >50) {
-        return 'healthy';
-      } else if (health >= 15) {
-        return 'wounded';
-      } else {
-        return 'critical';
-      }
+  if (health > 50) {
+    return 'healthy';
+  } if (health >= 15) {
+    return 'wounded';
+  }
+  return 'critical';
 }
